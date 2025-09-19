2e6f40b7cedb6257b3163862c97565a2888ddad0
2022184032-cmyk <2022184032@tukorea.ac.kr>
2025-09-19 22:46:46 +0900

    원운동 사각형 운동 뼈대 완성

--- 2DGP_Drill_03/character_grass.py
+++ 2DGP_Drill_03/character_grass.py
@@ -2,7 +2,20 @@ from pico2d import *
 
 open_canvas()
 
+
+def move_rectangle():
+    print("Moving rectangle")
+    pass
+
+
+def move_circle():
+    print("Moving circle")
+    pass
+
+
 while True:
+    move_rectangle()
+    move_circle()
     pass
 
 close_canvas()
